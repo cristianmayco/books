07bad97d7a4b7e0702c3edd05c86285a7c95b47f
Cristian Mayco da Costa <cristianmayco@gmail.com>
2020-06-27 21:44:59 -0300

    Removed search page and added search function on the home page

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,4 @@ from core import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('find', views.FindView.as_view(), name='find'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,22 +1,20 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import ListView
 from django.conf import settings
 import requests
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'index.html'
-
-
-class FindView(ListView):
-    template_name = 'find.html'
     context_object_name = 'books'
 
     def get_context_data(self, **kwargs):
-        context = super(FindView, self).get_context_data(**kwargs)
+        context = super(IndexView, self).get_context_data(**kwargs)
         return context
 
-    def get_queryset(self):
-        finder = settings.GOOGLE_BOOKS_API_BASE \
-                 + self.request.GET.get('query') \
-                 + '&key=AIzaSyCYagWGmWmKSzsynWSrzSPZP3Ts9NZec4M'
+    def get_queryset(self, *args, **kwargs):
+        finder = settings.GOOGLE_BOOKS_API_BASE
+        if self.request.GET.get('q') is None or self.request.GET.get('q') == '':
+            finder += 'Dom Casmurro'
+        else:
+            finder += self.request.GET.get('q') + '&key=AIzaSyCYagWGmWmKSzsynWSrzSPZP3Ts9NZec4M'
         return requests.get(finder).json()
